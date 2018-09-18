0c5da5d8dd6dca3ea2c4cf08d6b308e0d6cc781f
vasilstar <vasilstar97@gmail.com>
2018-09-18 08:35:31 +0500

    pdf

---
+++ site/templates/layouts/layout_pdf.php
@@ -0,0 +1,5 @@
+<section class="container pdf">
+  <img src="<?= $config->$urls->$templates.'assets/img/logo.svg' ?>" alt="" class="pdf__logo">
+  <p class="pdf__heading">Персональное предложение</p>
+  <img src="" alt="" class="pdf__img">
+</section>
